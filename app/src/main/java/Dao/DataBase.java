529a94bad3cafed665a369dd028dffffea330b93
Pony <1456719640@qq.com>
2017-07-28 20:23:05 +0800

    测试

--- app/src/main/java/Dao/DataBase.java
+++ app/src/main/java/Dao/DataBase.java
@@ -1,8 +1,54 @@
 package Dao;
 
+import com.guet.zigbee.Data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 /**
  * Created by Pony on 2017/7/28.
  */
 
 public class DataBase {
+    private static String URL="jdbc:mysql://119.23.45.116:3306/band?";
+    private static String USER="band";
+    private static String PASSWORD="band";
+
+    //sql语句
+    private String sql="select heart,x,y,lowblood,heightblood from data";
+
+    //Data的集合
+    ArrayList<Data> DataList=new ArrayList<Data>();
+
+public void TheSqlConnection()
+{
+    Connection conn;
+    try {
+        //加载驱动
+        Class.forName("com.mysql.jdbc.Driver");
+        //获得链接
+        conn= DriverManager.getConnection(URL,USER,PASSWORD);
+        Statement stmt=conn.createStatement();
+        ResultSet rs=stmt.executeQuery(sql);
+        while(rs.next())
+        {
+            Data data=new Data();
+            data.setHeart(rs.getString(1));
+            data.setX(String.valueOf(rs.getFloat(2)));
+            data.setY(String.valueOf(rs.getFloat(3)));
+            data.setBloodlow(String.valueOf(rs.getInt(4)));
+            data.setBloodhigh(String.valueOf(rs.getInt(5)));
+
+            //添加到Data集合
+            DataList.add(data);
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
+
 }
--- app/src/main/java/com/guet/zigbee/Data.java
+++ app/src/main/java/com/guet/zigbee/Data.java
@@ -1,8 +1,9 @@
 package com.guet.zigbee;
 
 /**
+ * Javabeans类，获取到的数据
  * Created by 尹文强 on 2017/5/11.
- * 从网站获取到的数据，http协议，json解析
+ * 从网站获取到的数据
  */
 
 public class Data {
@@ -11,9 +12,15 @@ public class Data {
     private String y;
     private String bloodhigh;
     private String bloodlow;
+
     public String getHeart() {
         return heart;
     }
+
+    public void setHeart(String heart) {
+        this.heart = heart;
+    }
+
     public String getX() {
         return x;
     }
@@ -34,7 +41,15 @@ public class Data {
         return bloodhigh;
     }
 
+    public void setBloodhigh(String bloodhigh) {
+        this.bloodhigh = bloodhigh;
+    }
+
     public String getBloodlow() {
         return bloodlow;
+    }
+
+    public void setBloodlow(String bloodlow) {
+        this.bloodlow = bloodlow;
     }
 }
